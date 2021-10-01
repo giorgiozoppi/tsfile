001516f3deb47ddfac254daa5665c6dedceadf10
Giorgio Zoppi <giorgio.zoppi@gmail.com>
2021-10-02 00:25:00 +0200

    Fix for the google style guide in c++ 	modified:   cmake/unit_test.cmake

--- cmake/unit_test.cmake
+++ cmake/unit_test.cmake
@@ -1,7 +1,7 @@
 # Create the unit testing executable.
 # By using glob, any test source files that are added to test/ will automatically
 # be added to the unit testing executable.
-file(GLOB_RECURSE TEST_SOURCE_FILES ${CMAKE_SOURCE_DIR}/test/*.cpp)
+file(GLOB_RECURSE TEST_SOURCE_FILES ${CMAKE_SOURCE_DIR}/test/*.cc)
 
 add_executable(unit_test ${SOURCE_FILES_NO_MAIN} ${TEST_SOURCE_FILES})
 # Enable CMake `make test` support.
